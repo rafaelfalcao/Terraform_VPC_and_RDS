2a0b4f29cb3714ff835327331beb4d55afc1a59c
r <rafael.falcao1@gmail.com>
2021-09-28 14:35:20 +0100

    added secrets, changed ports of ec2 ingress

--- TerraformChallenge/main.tf
+++ TerraformChallenge/main.tf
@@ -5,17 +5,17 @@ module "vpc" {
   #Classless Inter-Domain Routing 
   #IPv4 addresses for the vpc
   cidr = "192.168.0.0/16"
-  
-    #availability zones
-  azs             = ["eu-west-2a" , "eu-west-2b"] //, "eu-west-2c"]
+
+  #availability zones
+  azs = ["eu-west-2a", "eu-west-2b"] //, "eu-west-2c"]
 
   #SUBNETS - range of ip addresses in the vpc - subset of CIDR
 
-  private_subnets = ["192.168.1.0/24" , "192.168.2.0/24", "192.168.3.0/24"]
+  private_subnets = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
 
   #to route to an internet gateway
   #needs public ipv4 or elastic ip 
-  public_subnets  = ["192.168.100.0/24"] //, "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = ["192.168.100.0/24"] //, "10.0.102.0/24", "10.0.103.0/24"]
 
   #traffic routed to a vpn gateway for a site-to-site vpn connection
   #enables access to your remote network from your VPC by creating an AWS Site-to-Site VPN (Site-to-Site VPN) connection, 
@@ -23,28 +23,28 @@ module "vpc" {
   enable_vpn_gateway = true
 
   enable_nat_gateway = true
-  
+
 }
 
 ############################################  EC2 INSTANCE ##################################################
 resource "aws_instance" "myEC2" {
-    ami = "ami-069bc9cfa21be900c" # UBUNTU - ssh user is "ubuntu"
-    #LINUX2 AMI's user is ec2-user
-    instance_type= "t2.micro"
-    subnet_id     = module.vpc.public_subnets[0]
-    key_name = "personal_key"
-    associate_public_ip_address = true
-    vpc_security_group_ids = [ aws_security_group.ec2-sg.id ]
+  ami = "ami-069bc9cfa21be900c" # UBUNTU - ssh user is "ubuntu"
+  #LINUX2 AMI's user is ec2-user
+  instance_type               = "t2.micro"
+  subnet_id                   = module.vpc.public_subnets[0]
+  key_name                    = "personal_key"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.ec2-sg.id]
 
-    //passing user data to the instance 
-    //that can be used to perform common automated 
-    //configuration tasks and even run scripts after the instance starts. 
-    user_data = <<-EOF
+  //passing user data to the instance 
+  //that can be used to perform common automated 
+  //configuration tasks and even run scripts after the instance starts. 
+  user_data = <<-EOF
                 #!/bin/bash
                 sudo apt-get update
                 sudo apt-get install -y mysql-client
               EOF
-              #-y argument overrides the y/n question of apt-get
+  #-y argument overrides the y/n question of apt-get
 }
 
 #################################### AWS SSH KEY ######################################################
@@ -52,31 +52,58 @@ resource "aws_instance" "myEC2" {
 resource "aws_key_pair" "generated_key" {
   key_name   = "personal_key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwU8n52OUIiSk0P6XPkh/We3C7ClVBVOadrBCISm9c6NA3iGBdCwGuuyviKlGa0IvjebnE9OZMZ5AVG6wUbnmDyxQAIU2FXF6D2Pxb2Efp0/6FEw/B2TrIUiro2xO3DZTPMduQeFVCqTfWuoTNbzy4llOC5B4s0IGIBYDjMtxxmIS8eF5C0V6VdGx4l9UrlC2ZV4RVSZTMRuzBVrPpwubXaLgKYhWefvU7y0IVO3zFJq1inOaw3pmVklUlZ6ugCxFL+UJ+loIiIX/kgm03hbcPqyAt+JBjgCiofBqu75xDezz4YFiax1JKRTdE655D7i8ZAkHK9ep8l7A/NnuVL2oxSBVOXphmmcN06g8gnbcep8kOAaeMB9qR0RF8SSK5y43mSCJPFxHme97CiPz7BKMa8atEI+XcYJvucFK0xQ199qs0j+rspypL+NNsWS/vMtznHmFpVtjRShzqjsIpfbf+R9QXo5G0nfaCF3QeSnqi/zvav527rLWg5mAaIN/Qr7U= r@r-XPS-15-9570"
- #public key must be consumed from ENV var - bash profile file to load variables
+  #public key must be consumed from ENV var - bash profile file to load variables
 }
 
 
 ############################################  EC2 SECURITY GROUP ##################################################
 resource "aws_security_group" "ec2-sg" {
-    name = "security_group"
-    description = "allow outside access to EC2"
-    vpc_id = module.vpc.vpc_id
+  name        = "security_group"
+  description = "allow outside access to EC2"
+  vpc_id      = module.vpc.vpc_id
 
-    #traffic to enter
-    ingress {
-        protocol = -1 #all protocols
-        from_port = 0
-        to_port = 0
-        cidr_blocks =["0.0.0.0/0"]
-    }
-     
-    #traffic to exit
-    egress {
-        protocol = -1 #all protocols
-        from_port = 0
-        to_port = 0
-        cidr_blocks =["0.0.0.0/0"]
-    }
+  #traffic to enter 
+  #protocol = -1 -> all protocols
+  #RDS
+  ingress {
+    protocol    = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+    description = "allow connections from RDS"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    description = "allow ssh connections to my ec2"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    description = "HTTPS"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #traffic to exit
+  egress {
+    protocol    = -1 #all protocols
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 ############################################### ELASTIC IP  ###############################################################
@@ -92,19 +119,20 @@ resource "aws_security_group" "ec2-sg" {
 
 
 ################################################  RDS ########################################################3
- resource "aws_db_instance" "myDB" {
-    allocated_storage = 10
-    identifier = "rdsinstance"
-    engine = "mysql"
-    engine_version = "5.7"
-    instance_class = "db.t2.micro" #free tier
-    name = "rds"
-    username = "dbadmin"
-    password = "dbadmin123"
-    parameter_group_name = "default.mysql5.7"
-    db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
-    vpc_security_group_ids = [aws_security_group.rds-sg.id]
-  
+resource "aws_db_instance" "myDB" {
+  allocated_storage      = 10
+  identifier             = "rdsinstance"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro" #free tier
+  #sets secrets from variables
+  name                   = var.dbname
+  username               = var.db_username
+  password               = var.db_password
+  parameter_group_name   = "default.mysql5.7"
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds-sg.id]
+
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
@@ -118,25 +146,25 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 
 ############################################  RDS SECURITY GROUP ##################################################
 resource "aws_security_group" "rds-sg" {
-    name = "rds_security_group"
-    description = "allow outside access to RDS"
-    vpc_id = module.vpc.vpc_id
+  name        = "rds_security_group"
+  description = "allow outside access to RDS"
+  vpc_id      = module.vpc.vpc_id
 
-    #traffic to enter
-    ingress {
-        protocol = -1 #all protocols
-        from_port = 0
-        to_port = 0
-        cidr_blocks =["0.0.0.0/0"]
-    }
-     
-    #traffic to exit
-    egress {
-        protocol = -1 #all protocols
-        from_port = 0
-        to_port = 0
-        cidr_blocks =["0.0.0.0/0"]
-    }
+  #traffic to enter
+  ingress {
+    protocol    = -1 #all protocols
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #traffic to exit - allow all 
+  egress {
+    protocol    = -1 #all protocols
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
@@ -148,6 +176,6 @@ output "rds_endpoint" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.myEC2.public_ip 
+  value = aws_instance.myEC2.public_ip
 
 }
--- TerraformChallenge/variables.tf
+++ TerraformChallenge/variables.tf
@@ -3,18 +3,22 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-/* variable "db_name" {
+
+variable "dbname" {
   description = "RDS db_name"
-}
+} 
 
 variable "db_username" {
   description = "RDS db_username"
+  sensitive = true
 }
 
 variable "db_password" {
   description = "RDS db_password"
+  sensitive = true
 }
 
+/* 
 
 variable "instance_type" {
   description = "AWS instance_type"
@@ -26,4 +30,4 @@ variable "rds_instance_identifier_name" {
 
 variable "rds_cluster_identifier_name" {
   description = "RDS cluster_identifier_name"
-} */
+} */ 
